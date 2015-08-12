b9beeb3883b8d33fa363c8b41179dee879249c0a
Adam Dean <adam.dean@bravissimo.com>
2015-08-12 13:01:20 +0100

    A few tweaks to allow default files (index.html)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var _ = require('lodash');
+var _ = require('lodash'),
+    fs = require('fs'),
+    send = require('koa-send');
 
 module.exports = exports = function (directories, root) {
     if (!_.isArray(directories)) directories = [directories];
@@ -9,20 +11,23 @@ module.exports = exports = function (directories, root) {
     return function *(next) {
         var path = this.path;
         var isAsset = _.any(directories, function (dir) {
+            console.log(path + ' startsWith /' + dir + '?');
             return _.startsWith(path, '/' + dir);
         });
 
-        if (isAsset) {
-            try
-            {
-                var fd = fs.openSync(root + this.path, 'r');
-                yield send(this, root + this.path);
-                fs.close(fd);
-            }
-            catch(e)
-            {
-                console.log(e);
-            }
+        console.log('isAsset?', isAsset);
+        try
+        {
+            path = (isAsset && !fs.isDirectory(root + this.path))
+                ? root + this.path
+                : root + this.path + 'index.html';
+
+            console.log(path);
+
+            var fd = fs.openSync(path, 'r');
+            yield send(this, path);
+            fs.close(fd);
         }
+        catch(e) { console.log(e);/* 404 */ }
     }
 };
