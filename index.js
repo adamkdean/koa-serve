afc7588aa71208872135f0419d4f9e3e11a0ff29
Adam Dean <adam.dean@bravissimo.com>
2015-08-13 12:41:05 +0100

    Fixed issue with pathing

--- index.js
+++ index.js
@@ -2,28 +2,29 @@
 
 var _ = require('lodash'),
     fs = require('fs'),
+    path = require('path'),
     send = require('koa-send');
 
 module.exports = exports = function (directories, root) {
     if (!_.isArray(directories)) directories = [directories];
-    root = root || __dirname;
+    root = root || path.join(__dirname, '..', '..');
 
     return function *(next) {
-        var path = this.path,
-            isAsset, fd;
+        var reqPath = this.path,
+            filePath, isAsset, fd;
 
         isAsset = _.any(directories, function (dir) {
-            return _.startsWith(path, '/' + dir);
+            return _.startsWith(reqPath, '/' + dir);
         });
 
         try
         {
-            path = (isAsset && !fs.lstatSync(root + this.path).isDirectory())
+            filePath = (isAsset && !fs.lstatSync(root + this.path).isDirectory())
                 ? root + this.path
                 : root + this.path + 'index.html';
 
-            fd = fs.openSync(path, 'r');
-            yield send(this, path);
+            fd = fs.openSync(filePath, 'r');
+            yield send(this, filePath);
             fs.close(fd);
         }
         catch (e)
