cb8c57488160466cbfc504a696147c757503cb9c
satanas <wil.alejandro@gmail.com>
2015-09-25 18:57:20 -0300

    Fixed error when trying to serve files without passing root. Added debugging output

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 var _ = require('lodash'),
     fs = require('fs'),
     path = require('path'),
-    send = require('koa-send');
+    send = require('koa-send'),
+    debug = require('debug')('koa-serve');
 
 module.exports = exports = function (directories, root) {
     if (!_.isArray(directories)) directories = [directories];
-    root = path.normalize(root) || path.join(__dirname, '..', '..');
+    root = root || path.join(__dirname, '..', '..');
+    root = path.normalize(root);
 
     return function *(next) {
         var reqPath = this.path,
@@ -17,18 +19,19 @@ module.exports = exports = function (directories, root) {
             return _.startsWith(reqPath, '/' + dir);
         });
 
+        debug('requesting', reqPath);
+        this.path = root + this.path;
         try
         {
-            filePath = (isAsset && !fs.lstatSync(root + this.path).isDirectory())
-                ? root + this.path
-                : root + this.path + 'index.html';
+            filePath = (isAsset && !fs.lstatSync(this.path).isDirectory())
+                ? this.path
+                : this.path + 'index.html';
 
-            fd = fs.openSync(filePath, 'r');
             yield send(this, filePath);
-            fs.close(fd);
         }
         catch (e)
         {
+            debug(e);
             if (isAsset) {
                 this.body = 'Not Found';
                 this.status = 404;
